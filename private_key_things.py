6dd33858389a4eee9e63fff6f16d2506bd9767fc
yakuhito <y@kuhi.to>
2023-02-26 20:44:11 -0800

    checkpoint

---
+++ private_key_things.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import asyncio
+from pathlib import Path
+from typing import List
+from chia.wallet.sign_coin_spends import sign_coin_spends
+from blspy import PrivateKey, AugSchemeMPL
+from chia.types.blockchain_format.coin import Coin
+from chia.types.blockchain_format.program import Program
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.types.condition_opcodes import ConditionOpcode
+from chia.util.hash import std_hash
+from chia.util.ints import uint64
+from clvm.casts import int_to_bytes
+from cdv.cmds.rpc import get_client
+from chia.wallet.puzzles.load_clvm import load_clvm
+from chia.wallet.puzzles.singleton_top_layer_v1_1 import launch_conditions_and_coinsol, pay_to_singleton_puzzle, SINGLETON_MOD_HASH, SINGLETON_MOD, P2_SINGLETON_MOD, SINGLETON_LAUNCHER_HASH, SINGLETON_LAUNCHER, lineage_proof_for_coinsol, puzzle_for_singleton, solution_for_singleton, generate_launcher_coin
+from chia.wallet.puzzles.p2_delegated_puzzle_or_hidden_puzzle import puzzle_for_pk, calculate_synthetic_secret_key, DEFAULT_HIDDEN_PUZZLE_HASH, puzzle_for_synthetic_public_key, solution_for_delegated_puzzle
+from chia.wallet.puzzles.cat_loader import CAT_MOD_HASH, CAT_MOD
+from chia.wallet.trading.offer import OFFER_MOD_HASH, OFFER_MOD
+from chia.rpc.full_node_rpc_client import FullNodeRpcClient
+from chia.rpc.wallet_rpc_client import WalletRpcClient
+from cdv.cmds.sim_utils import SIMULATOR_ROOT_PATH
+from chia.simulator.simulator_full_node_rpc_client import SimulatorFullNodeRpcClient
+from chia.util.config import load_config
+from chia.util.ints import uint16, uint32
+from chia.wallet.derive_keys import master_sk_to_wallet_sk_unhardened
+from chia.consensus.default_constants import DEFAULT_CONSTANTS
+from chia.types.coin_spend import CoinSpend
+from chia.wallet.cat_wallet.cat_utils import construct_cat_puzzle
+from chia.wallet.puzzles.tails import GenesisById
+from chia.wallet.puzzles.cat_loader import CAT_MOD, CAT_MOD_HASH
+from chia_rs import run_chia_program
+from chia.types.blockchain_format.program import INFINITE_COST, Program
+from chia.util.condition_tools import conditions_dict_for_solution
+from chia.wallet.lineage_proof import LineageProof
+from chia.wallet.cat_wallet.cat_utils import (
+    SpendableCAT,
+    construct_cat_puzzle,
+    unsigned_spend_bundle_for_spendable_cats,
+)
+from chia.wallet.trading.offer import Offer
+from chia.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
+from chia.types.spend_bundle import SpendBundle
+from chia.util.bech32m import bech32_decode, bech32_encode, convertbits
+from chia.wallet.util.puzzle_compression import (
+    compress_object_with_puzzles,
+    decompress_object_with_puzzles,
+    lowest_best_version,
+)
+from chia.wallet.puzzles.p2_conditions import puzzle_for_conditions
+from chia.util.hash import std_hash
+
+async def get_private_key_DO_NOT_CALL_OUTSIDE_THIS_FILE(wallet_client):
+    fingerprint = await wallet_client.get_logged_in_fingerprint()
+
+    sk_resp = await wallet_client.get_private_key(fingerprint)
+    sk_hex = sk_resp['sk']
+    return PrivateKey.from_bytes(bytes.fromhex(sk_hex))
+
+async def get_standard_coin_puzzle(wallet_client, std_coin):
+    master_sk = await get_private_key_DO_NOT_CALL_OUTSIDE_THIS_FILE(wallet_client)
+
+    i = 0
+    while i < 10000:
+        wallet_sk = master_sk_to_wallet_sk_unhardened(master_sk, i)
+        synth_secret_key = calculate_synthetic_secret_key(wallet_sk, DEFAULT_HIDDEN_PUZZLE_HASH)
+        synth_key = synth_secret_key.get_g1()
+        puzzle = puzzle_for_synthetic_public_key(synth_key)
+        puzzle_hash = puzzle.get_tree_hash()            
+        if puzzle_hash == std_coin.puzzle_hash:
+            return puzzle
+        i += 1
+
+    return None
+
+async def sign_spend_bundle(wallet_client, sb, no_max_keys = 1):
+    master_sk = await get_private_key_DO_NOT_CALL_OUTSIDE_THIS_FILE(wallet_client)
+
+    puzzle_hashes = [c.coin.puzzle_hash for c in sb.coin_spends]
+    keys_used = 0
+    i = 0
+    while i < 10000:
+        wallet_sk = master_sk_to_wallet_sk_unhardened(master_sk, i)
+        synth_secret_key = calculate_synthetic_secret_key(wallet_sk, DEFAULT_HIDDEN_PUZZLE_HASH)
+        synth_key = synth_secret_key.get_g1()
+        puzzle = puzzle_for_synthetic_public_key(synth_key)
+        puzzle_hash = puzzle.get_tree_hash()            
+        if puzzle_hash in puzzle_hashes:
+            keys_used += 1
+            async def pk_to_sk(pk):
+                return synth_secret_key
+
+            sig_old = sb.aggregated_signature
+            sb = await sign_coin_spends(
+                sb.coin_spends,
+                pk_to_sk,
+                DEFAULT_CONSTANTS.AGG_SIG_ME_ADDITIONAL_DATA,
+                DEFAULT_CONSTANTS.MAX_BLOCK_COST_CLVM,
+            )
+
+            new_agg_sig = AugSchemeMPL.aggregate([sig_old, sb.aggregated_signature])
+            sb = SpendBundle(sb.coin_spends, new_agg_sig)
+
+        if keys_used >= no_max_keys:
+            return sb
+        i += 1
+
+    return sb
--- tibet.py
+++ tibet.py
@@ -2,66 +2,166 @@ import click
 import json
 import sys
 import os
+import asyncio
+from tibet_lib import *
+from private_key_things import *
 
 @click.group()
 def cli():
     pass
 
+cached_config = None
+
+def get_config_item(*args):
+    global cached_config
+    if cached_config is None:
+        get_config()
+    
+    ret = cached_config
+    for arg in args:
+        if ret is None:
+            click.echo(f"Could not find {'->'.join(args)} - is this an error on your part?")
+            return ret
+        ret = ret[arg]
+    
+    return ret
 
 def get_config():
-    try:
-        config = json.loads(open("config.json", "r").read())
-        return config
-    except:
-        open("config.json", "w").write("{}")
-        return {}
+    global cached_config
+    if cached_config is None:
+        try:
+            cached_config = json.loads(open("config.json", "r").read())
+        except:
+            open("config.json", "w").write("{}")
+            cached_config = {}
+        
+    return cached_config
 
 
 def save_config(config):
+    cached_config = config
     open("config.json", "w").write(json.dumps(config, sort_keys=True, indent=4))
 
 
 @click.command()
 @click.option('--chia-root', default=None, help='Chia root directory (e.g., ~/.chia/mainnet)')
-@click.option('--full-node-rpc-port', default=None, help='Chia wallet RPC port')
-@click.option('--wallet-rpc-port', default=None, help='Chia full node RPC port')
 @click.option('--use-preset', default='custom', type=click.Choice(['custom', 'simulator', 'testnet10', 'mainnet'], case_sensitive=False))
-def config_node(chia_root, full_node_rpc_port, wallet_rpc_port, use_preset):
+def config_node(chia_root, use_preset):
     if use_preset == 'custom' and (chia_root is None or full_node_rpc_port is None or wallet_rpc_port is None):
         print("Use a preset or fill out all options.")
         sys.exit(1)
     
     if use_preset == "mainnet":
         chia_root = "~/.chia/mainnet"
-        full_node_rpc_port = 8555
-        wallet_rpc_port = 9256
     elif use_preset == "testnet10":
         chia_root = "~/.chia/testnet10"
-        full_node_rpc_port = 8555
-        wallet_rpc_port = 9256 
     elif use_preset == "simulator":
         chia_root = "~/.chia/simulator/main"
-        full_node_rpc_port = 10568
-        wallet_rpc_port = 11269
-
+    
     chia_root = os.path.expanduser(chia_root)
 
-    config_node_obj = {
-        "chia_root": chia_root,
-        "full_node_rpc_port": full_node_rpc_port,
-        "wallet_rpc_port": wallet_rpc_port
-    }
     config = get_config()
-    config["node"] = config_node_obj
+    config["chia_root"] = chia_root
     save_config(config)
     click.echo("Config updated and saved successfully.")
 
 
 @click.command()
-def launch_router():
-    click.echo("Launching router!")
+def test_node_config():
+    asyncio.run(_test_node_config())
+
+async def _test_node_config():
+    full_node_client = await get_full_node_client(get_config_item("chia_root"))
+    full_node_client_status = await full_node_client.healthz()
+    click.echo("full node client... " + str(full_node_client_status))
+
+    full_node_client.close()
+    await full_node_client.await_closed()
+
+    wallet_client = await get_wallet_client(get_config_item("chia_root"))
+    wallet_client_status = await wallet_client.healthz()
+    click.echo("wallet client... " + str(wallet_client_status))
+
+    wallet_client.close()
+    await wallet_client.await_closed()
+
+
+@click.command()
+@click.option("--push-tx", is_flag=True, show_default=True, default=False, help="Push the signed spend bundle to the network and update launcher is in config.")
+def launch_router(push_tx):
+    asyncio.run(_launch_router(push_tx))
+
+async def _launch_router(push_tx):
+    wallet_client = await get_wallet_client(get_config_item("chia_root"))
+
+    coins = await wallet_client.select_coins(2, 1) # wallet id 1, amount 2
+    if len(coins) > 1:
+        click.echo("Really? You have a one-mojo coin?!")
+        sys.exit(1)
+
+    coin = coins[0]
+    coin_puzzle = await get_standard_coin_puzzle(wallet_client, coin)
+
+    click.echo(f"Using coin 0x{coin.name().hex()}...")
+
+    launcher_id, sb = await launch_router_from_coin(coin, coin_puzzle)
+    click.echo(f"Router launcher id: {launcher_id}")
+
+    signed_sb = await sign_spend_bundle(wallet_client, sb)
+
+    if push_tx:
+        click.echo(f"Pushing tx...")
+        full_node_client = await get_full_node_client(get_config_item("chia_root"))
+        resp = await full_node_client.push_tx(signed_sb)
+        click.echo(resp)
+        full_node_client.close()
+        await full_node_client.await_closed()
+
+        click.echo("Saving config...")
+        config = get_config()
+        config["router_launcher_id"] = launcher_id
+        config["router_last_processed_id"] = ""
+        save_config(config)
+        click.echo("Done.")
+        click.echo("Don't forget to sync-pairs now!")
+    else:
+        click.echo("Here's your spend bundle:")
+        click.echo(str(signed_sb))
+        click.echo("Use --push-tx to push this spend.")
+
+    wallet_client.close()
+    await wallet_client.await_closed()
+
+@click.command()
+def launch_test_token():
+    pass # todo
+
+@click.command()
+def launch_pair():
+    pass # todo
+
+@click.command()
+def sync_pairs():
+    asyncio.run(_sync_pairs())
+
+async def _sync_pairs():
+    router_last_processed_id = get_config_item("router_last_processed_id")
+    if router_last_processed_id is None or len(router_last_processed_id) != 32:
+        router_last_processed_id = get_config_item("router_launcher_id")
+        if router_last_processed_id is None:
+            click.echo("No router launcher id. Please either set it or launch a new router.")
+            os.exit(1)
+    
+    full_node_client = await get_full_node_client(get_config_item("chia_root"))
+    resp = await full_node_client.get_blockchain_state()
+    print(resp) # todo
+    full_node_client.close()
+    await full_node_client.await_closed()
 
 if __name__ == "__main__":
     cli.add_command(config_node)
+    cli.add_command(test_node_config)
     cli.add_command(launch_router)
+    cli.add_command(launch_pair)
+    cli.add_command(sync_pairs)
     cli()
