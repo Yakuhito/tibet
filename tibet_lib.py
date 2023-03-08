078cb3c2b16801aab957937752f40920b4e8d60c
yakuhito <y@kuhi.to>
2023-03-08 12:58:25 -0800

    should work now (upgraded wrong file)

--- tibet_lib.py
+++ tibet_lib.py
@@ -14,7 +14,6 @@ from chia.util.hash import std_hash
 from chia.util.ints import uint64
 from clvm.casts import int_to_bytes
 from cdv.cmds.rpc import get_client
-from chia.wallet.puzzles.load_clvm import load_clvm
 from chia.wallet.puzzles.singleton_top_layer_v1_1 import launch_conditions_and_coinsol, SINGLETON_MOD_HASH, SINGLETON_MOD, SINGLETON_LAUNCHER_HASH, SINGLETON_LAUNCHER, lineage_proof_for_coinsol, puzzle_for_singleton, solution_for_singleton, generate_launcher_coin
 from chia.wallet.puzzles.p2_delegated_puzzle_or_hidden_puzzle import puzzle_for_pk, calculate_synthetic_secret_key, DEFAULT_HIDDEN_PUZZLE_HASH, puzzle_for_synthetic_public_key, solution_for_delegated_puzzle
 from chia.wallet.puzzles.cat_loader import CAT_MOD_HASH, CAT_MOD
@@ -33,6 +32,7 @@ from chia.wallet.puzzles.cat_loader import CAT_MOD, CAT_MOD_HASH
 from chia_rs import run_chia_program
 from chia.types.blockchain_format.program import INFINITE_COST, Program
 from chia.util.condition_tools import conditions_dict_for_solution
+from chia.types.blockchain_format.program import SerializedProgram
 from chia.wallet.lineage_proof import LineageProof
 from chia.wallet.cat_wallet.cat_utils import (
     SpendableCAT,
@@ -52,10 +52,19 @@ from chia.wallet.util.puzzle_compression import (
 from chia.wallet.puzzles.p2_conditions import puzzle_for_conditions
 from chia.util.hash import std_hash
 
-ROUTER_MOD: Program = load_clvm("../../../../../../../clvm/router.clvm", recompile=False)
-PAIR_MOD: Program = load_clvm("../../../../../../../clvm/pair.clvm", recompile=False)
-LIQUIDITY_TAIL_MOD: Program = load_clvm("../../../../../../../clvm/liquidity_tail.clvm", recompile=False)
-P2_SINGLETON_FLASHLOAN_MOD: Program = load_clvm("../../../../../../../clvm/p2_singleton_flashloan.clvm", recompile=False)
+def load_clvm_hex(
+    filename
+) -> SerializedProgram:
+    clvm_hex = open(filename, "r").read().strip()
+    assert len(clvm_hex) != 0
+    
+    clvm_blob = bytes.fromhex(clvm_hex)
+    return SerializedProgram.from_bytes(clvm_blob)
+
+ROUTER_MOD: Program = load_clvm_hex("clvm/router.clvm.hex")
+PAIR_MOD: Program = load_clvm_hex("clvm/pair.clvm.hex")
+LIQUIDITY_TAIL_MOD: Program = load_clvm_hex("clvm/liquidity_tail.clvm.hex")
+P2_SINGLETON_FLASHLOAN_MOD: Program = load_clvm_hex("clvm/p2_singleton_flashloan.clvm.hex")
 
 ROUTER_MOD_HASH = ROUTER_MOD.get_tree_hash()
 PAIR_MOD_HASH = PAIR_MOD.get_tree_hash()
